938f256374b6748c89c3feb464cbf5db5c07ae9f
catid <mrcatid@gmail.com>
2024-01-16 08:34:50 +0000

    share lru

--- rl/models.py
+++ rl/models.py
@@ -23,23 +23,46 @@ def get_embedding_dim():
 
 
 ################################################################################
-# Actor Model
+# Observation Model
 
 from lru import LRU
 
-# Action scoring model scores a batch of potential actions given a list of
-# observations, assigning a number -1..1 to each action.
-class ActionScoringModel(nn.Module):
-    def __init__(self, d_embed=384, d_obs_hidden=64, d_obs_out=64, d_mlp=64):
+# Learns latents from a list of observation strings (provided as embeddings)
+class ObservationModel(nn.Module):
+    def __init__(self, d_embed=384, d_obs_hidden=64, d_obs_latents=64):
         super().__init__()
 
         # Encodes a variable-length set of observation sentences (as embeddings)
         # into a learned latent space.
-        self.observation_head = LRU(d_in=d_embed, d_hidden=d_obs_hidden, d_out=d_obs_out)
+        self.observation_head = LRU(d_in=d_embed, d_hidden=d_obs_hidden, d_out=d_obs_latents)
+
+    # observations_embeddings: tensor of shape (batch, len(observations), d_embed)
+    # actions_embeddings: tensor of shape (batch, len(actions), d_embed)
+    # Returns (batch, d_obs_latents)
+    def forward(self, observations_embeddings):
+        # Latents for observations (batch, d_obs_latents)
+        latent_obs = self.observation_head(observations_embeddings)
+
+        # Max-pool the latents (batch, d_obs_latents)
+        # The idea here is to simply keep the strongest latent signals from each
+        # observation, with the intuition that each signal may correspond to a
+        # learned condition
+        latent_obs, _ = torch.max(latent_obs, dim=1)
+
+        return latent_obs
+
+
+################################################################################
+# Action Head Model
+
+# Action scoring model: Scores actions given ObservationModel latents.
+class ActionHeadModel(nn.Module):
+    def __init__(self, d_embed=384, d_obs_latents=64, d_mlp=64):
+        super().__init__()
 
         # Learned scoring 
         self.action_score_network = nn.Sequential(
-            nn.Linear(d_embed + d_obs_out, d_mlp),
+            nn.Linear(d_embed + d_obs_latents, d_mlp),
             nn.Tanh(),
             nn.Linear(d_mlp, d_mlp//2),
             nn.Tanh(),
@@ -49,18 +72,9 @@ class ActionScoringModel(nn.Module):
 
     # observations_embeddings: tensor of shape (batch, len(observations), d_embed)
     # actions_embeddings: tensor of shape (batch, len(actions), d_embed)
-    def forward(self, observations_embeddings, actions_embeddings):
-        # Latents for observations (batch, d_obs_out)
-        latent_obs = self.observation_head(observations_embeddings)
-
-        # Max-pool the latents (batch, d_obs_out)
-        # The idea here is to simply keep the strongest latent signals from each
-        # observation, with the intuition that each signal may correspond to a
-        # learned condition
-        latent_obs, _ = torch.max(latent_obs, dim=1)
-
-        # Expand the latent obs so we duplicate it so there's one per action
-        # Combine latent_obs and actions_embeddings
+    def forward(self, latent_obs, actions_embeddings):
+        # Expand the latent obs so we duplicate it so there's one per action.
+        # Combine latent_obs and actions_embeddings.
         inputs = torch.cat((latent_obs.unsqueeze(1).expand(-1, actions_embeddings.size(1), -1), actions_embeddings), dim=-1)
 
         # Produce one score per action per batch (batch, len(actions), 1)
@@ -68,23 +82,19 @@ class ActionScoringModel(nn.Module):
 
 
 ################################################################################
-# Critic Model
+# Critic Head Model
 
 from lru import LRU
 
 # Critic model is modeling expectation of reward to check if we're improving our
 # expectation of reward during online training.
-class ObservationExpectationModel(nn.Module):
-    def __init__(self, d_embed=384, d_obs_hidden=64, d_obs_out=48, d_mlp=32):
+class CriticHeadModel(nn.Module):
+    def __init__(self, d_obs_latents=64, d_mlp=64):
         super().__init__()
-
-        # Encodes a variable-length set of observation sentences (as embeddings)
-        # into a learned latent space.
-        self.observation_head = LRU(d_in=d_embed, d_hidden=d_obs_hidden, d_out=d_obs_out)
 
         # Learned scoring 
         self.expected_score_network = nn.Sequential(
-            nn.Linear(d_embed + d_obs_out, d_mlp),
+            nn.Linear(d_obs_latents, d_mlp),
             nn.Tanh(),
             nn.Linear(d_mlp, d_mlp//2),
             nn.Tanh(),
@@ -92,21 +102,7 @@ class ObservationExpectationModel(nn.Module):
             nn.Tanh(),
         )
 
-    # observations_embeddings: tensor of shape (batch, len(observations), d_embed)
-    # actions_embeddings: tensor of shape (batch, len(actions), d_embed)
-    def forward(self, observations_embeddings, actions_embeddings):
-        # Latents for observations (batch, len(obs), d_obs_out)
-        latent_obs = self.observation_head(observations_embeddings)
-
-        # Max-pool the latents (batch, d_obs_out)
-        # The idea here is to simply keep the strongest latent signals from each
-        # observation, with the intuition that each signal may correspond to a
-        # learned condition
-        latent_obs, _ = torch.max(latent_obs, dim=1)
-
-        # Expand the latent obs so we duplicate it so there's one per action
-        # Combine latent_obs and actions_embeddings
-        inputs = torch.cat((latent_obs.unsqueeze(1).expand(-1, actions_embeddings.size(1), -1), actions_embeddings), dim=-1)
-
-        # Produce one score per action per batch (batch, len(actions), 1)
-        return self.expected_score_network(inputs)
+    # latent_obs: (batch, d_obs_latents)
+    # Returns (batch, 1)
+    def forward(self, latent_obs):
+        return self.expected_score_network(latent_obs)
